e528815afe0d5ad06003435a9ec7c36c85f50a90
RomeoAxpuac <misinicialesbray@gmail.com>
2016-08-04 20:54:30 -0600

    Creando el archivo y las carpetas, al fin xD

--- Proyectoc.c
+++ Proyectoc.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+
+
 
 int main()
 {
+
+char linea[] = "/home/romoeoaxpuac/Escritorio/RebecaCaradeTortuca";
+    char separador[] = "\/";
+
+    char *trozo = NULL;
+
+    trozo = strtok( linea, separador);
+
+    while( trozo != NULL ) {
+        printf( "Trozo : %s \n", trozo);
+        trozo = strtok( NULL, separador);
+    }
+
 /*
 //wb+
 FILE * f;
 f = fopen("/home/romoeoaxpuac/Escritorio/Disco1.dsk","wb+");
 fclose(f);
 */
+
+
+
+system("mkdir /home/romoeoaxpuac/Escritorio/RebecaCaradeTortuca");
 FILE *fp;
 
- 	char cadena[] = " ";
+ 	char cadena[] = "/home/romoeoaxpuac/Escritorio/SoyLaTos";
 
- 	fp = fopen ("/home/romoeoaxpuac/Escritorio/Disco1.dsk","wb+");
+ 	fp = fopen ("/home/romoeoaxpuac/Escritorio/Disco2.dsk","wb+");
 
  	int a= 0;
  	for(a;a<100;a++){
@@ -21,22 +43,7 @@ FILE *fp;
     }
  	fclose ( fp );
  	////
- 	for(a;a<100;a++){
- 	fputs( cadena, fp );
-    }
- 	fclose ( fp );
- 	for(a;a<100;a++){
- 	fputs( cadena, fp );
-    }
- 	fclose ( fp );
- 	for(a;a<100;a++){
- 	fputs( cadena, fp );
-    }
- 	fclose ( fp );
- 	for(a;a<100;a++){
- 	fputs( cadena, fp );
-    }
- 	fclose ( fp );
+
 
 
         printf("Hola mundo");
