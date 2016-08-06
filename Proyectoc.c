2273dbaad9c9fda18a7f5c181879af70a905c1be
RomeoAxpuac <misinicialesbray@gmail.com>
2016-08-05 23:15:00 -0600

    Verificando Existencia de Ficheros

--- Proyectoc.c
+++ Proyectoc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <string.h>
 
 
@@ -17,6 +18,7 @@ int main()
 /// aca van los parametros ///
 char archivo[]="/home/romoeoaxpuac/Escritorio/Discoxxxx.dsk";
 char archivo2[]="/home/romoeoaxpuac/Escritorio/Discoxxxx.dsk";
+char archivo3[]="/home/romoeoaxpuac/Escritorio/Discoxxxx.dsk";
 int size = 24; // Tamaño del archivo
 char tipo = 'c';
 
@@ -31,12 +33,15 @@ if(size >0){
 
 char separador[] = "\/";
         char h [] = "";
+        char h2 [] = "/";
+        char * trozo2;
         char * trozo;
         int valor = 0;
         trozo = strtok( archivo, separador);
 
         while( trozo != NULL ) {
         strcpy(h,trozo);
+        strcat(h2,h);
         //printf(h);
         //printf( "Trozo : %s \n", trozo);//<- buscando .dsk
         trozo = strtok( NULL, separador);
@@ -45,7 +50,23 @@ char separador[] = "\/";
         if ( (strstr(h,".dsk") == NULL) && (strstr(h,".DSK") == NULL) ){
             puts("El Disco no se puede crear, el comando se encuentra en defecto");
         }else {
-            //CREAMOS EL OBJETO DE ARCHIVOS Y VEMOS SI NO HA SIDO CREADO CON anterioridad
+
+            puts(h2);
+
+        /*
+
+                //verificamos la existencia del directorio
+                DIR * directorio;
+                struct dirent * vacio;
+                directorio = opendir("/home/romoeoaxpuac/Escritorio/PACK");
+                if(directorio){
+                    puts("hola");
+                    closedir(directorio);
+                }else{
+                    puts("adios");
+                }
+        */
+           //CREAMOS EL OBJETO DE ARCHIVOS Y VEMOS SI NO HA SIDO CREADO CON anterioridad
             FILE *fpx = fopen(archivo2,"r");
                 if( fpx != NULL) {
                     printf("Error, El Disco Ya Existe");
